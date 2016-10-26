3de25d3ea1cc75f4dadf88be371ae8a2429a83ab
William Guimarães <willian.fguim@gmail.com>
2016-10-25 23:44:40 -0200

    Adicionado marcador do entregador com localização enviada pelo pusher

--- ionic/www/js/controllers/client/viewDelivery.js
+++ ionic/www/js/controllers/client/viewDelivery.js
@@ -7,8 +7,8 @@ function($scope, $stateParams, $ionicLoading, ClientOrder, $localStorage, $ionic
 	$scope.order = {};
 	$scope.map = {
 		center: {
-			latitude: -30.0806133,
-			longitude: -51.1729395
+			latitude: 0,
+			longitude: 0
 		},
 		zoom: 16
 	};
@@ -30,6 +30,12 @@ function($scope, $stateParams, $ionicLoading, ClientOrder, $localStorage, $ionic
 	},
 	function(){
 		$ionicLoading.hide();
+	});
+	
+	$scope.$watch('markers.length', function(value){
+		if(value == 2){
+			createBounds();
+		}
 	});
 	
 	function initMarkers(order){
@@ -73,9 +79,52 @@ function($scope, $stateParams, $ionicLoading, ClientOrder, $localStorage, $ionic
 		var pusher = $pusher($window.client),
 			channel = pusher.subscribe(channel);
 		var handler = function(data){
-			console.log(data);
+			var iconUrl = 'http://maps.google.com/mapfiles/kml/pal2/';
+			var lat = data.geo.lat, long = data.geo.long;
+			if($scope.markers.length == 1 || $scope.markers.length == 0){
+				$scope.markers.push({
+					id: 'entregador',
+					coords: {
+						latitude: lat,
+						longitude: long
+					},
+					options: {
+						title: 'Localização do etregador.',
+						icon: iconUrl + 'icon47.png'
+					}
+				});
+				return;
+			}
+			
+			for(var key in $scope.markers){
+				if($scope.markers[key].id == 'entregador'){
+					$scope.markers[key].coords = {
+							latitude: lat,
+							longitude: long
+					}
+				}
+			}
 		};
 		channel.bind('CodeDelivery\\Events\\GetLocationDeliveryMan', handler);
+	};
+	
+	function createBounds(){
+		var bounds = new google.maps.LatLngBounds(),
+			latlng;
+		angular.forEach($scope.markers, function(value){
+			latlng = new google.maps.LatLng(Number(value.coords.latitude), Number(value.coords.longitude));
+			bounds.extend(latlng);
+		});
+		$scope.map.bounds = {
+			northeast: {
+				latitude: bounds.getNorthEast().lat(),
+				longitude: bounds.getNorthEast().lng()
+			},
+			southwest: {
+				latitude: bounds.getSouthWest().lat(),
+				longitude: bounds.getSouthWest().lng()
+			}
+		};
 	}
 	
 }])
